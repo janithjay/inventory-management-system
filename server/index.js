7f3af7c645187a48db5990d155ff67013d318755
janith <janithjayashan018@gmail.com>
2024-12-14 14:15:26 +0530

    updated api and data fetching

--- server/index.js
+++ server/index.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
-// bahasuru added for register and login
-
 
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -115,6 +112,17 @@ app.post('/products', async (req, res) => {
   } catch (error) {
     console.error('Error saving product:', error);
     res.status(500).json({ error: 'Failed to create product' });
+  }
+});
+
+// Get all products Route
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find(); // Fetch all products from the database
+    res.status(200).json(products); // Send the products as a response
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Failed to fetch products' });
   }
 });
 
